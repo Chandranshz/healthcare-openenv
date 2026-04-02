8a4be59e6836da87df95f19b19480b8abe4c77f4
Chandranshz <chandranshyadav59@gmail.com>
2026-04-03 04:08:25 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from inference import run_step
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Healthcare OpenEnv Running"}
+
+@app.post("/step")
+def step(action: dict):
+    return run_step(action)
+
+@app.post("/reset")
+def reset():
+    return {"status": "reset done"}
+
+@app.get("/state")
+def state():
+    return {"state": "running"}
+
+
+def main():
+    return app
